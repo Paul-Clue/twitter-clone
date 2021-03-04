04cf5c45af52c47e92b8227f986f68cdc1a30789
unknown <paulclue20@gmail.com>
2021-03-04 15:06:42 -0500

    fix sign-up issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,15 @@
-class RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController 
 
-  private
+	
 
-  def sign_up_params
-    params.require(:user).permit(:name, :username, email, :password, :password_confirmation)
-  end
+	private
 
-  def account_update_params
-    params.require(:user).permit(:name, :username, email, :password, :password_confirmation, :current_password)
-  end
+	def sign_up_params 
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+	end
+
+	def acount_update_params 
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+	end
+
 end
